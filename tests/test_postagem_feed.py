8c5539cfdfe9f21d73b114247d524cc9f62e391d
Sofia Koppe <koppesofia4@gmail.com>
2025-07-02 19:23:54 -0300

    teste postagem artigo

--- tests/test_postagem_feed.py
+++ tests/test_postagem_feed.py
@@ -3,12 +3,11 @@ import sys
 from data.postagem_feed_repo import *
 from data.postagem_feed_model import PostagemFeed
 from data.usuario_model import Usuario
-import data.usuario_repo as usuario_repo
-import data.tutor_repo as tutor_repo
-import data.postagem_feed_repo as postagem_repo
-from data.usuario_model import Usuario
+from data import usuario_repo, usuario_sql
+from data import tutor_repo
+from data import postagem_feed_repo
 from data.tutor_model import Tutor
-from data.postagem_feed_model import PostagemFeed
+
 
 class TestPostagemFeedRepo:
     def test_criar_tabela(self, test_db):
@@ -20,41 +19,26 @@ class TestPostagemFeedRepo:
 
     def test_inserir_postagem_feed(self, test_db):
 
-        
         # 1. Criar tabelas na ordem correta
         usuario_repo.criar_tabela_usuario()
-        tutor_repo.criar_tabela_tutor()
-        postagem_repo.criar_tabela()
-        
-        # 2. Inserir usuário
-        usuario = Usuario(
-            id_usuario=0,
-            nome="Usuario Teste",
-            email="usuario@teste.com",
-            senha="12345678",
-            telefone="11999999999"
-        )
-        id_usuario = usuario_repo.inserir_usuario(usuario)
-        assert id_usuario is not None, "Falha ao inserir usuário"
+        tutor_repo.criar_tabela_tutor()  # Assumindo que existe
+        postagem_feed_repo.criar_tabela()
         
         # 3. Inserir tutor
         tutor = Tutor(
-            id_usuario=id_usuario,
+            id_usuario=0,
             nome="Tutor Teste",
             email="tutor@teste.com",
             senha="12345678",
             telefone="11999999999"
         )
-        id_tutor = tutor_repo.inserir_tutor(tutor)
-        assert id_tutor is not None, "Falha ao inserir tutor"
+        tutor = tutor_repo.inserir_tutor(tutor)  # Use a função correta do tutor_repo
+        assert tutor == None, "Falha ao inserir tutor"
         
-        # 4. ✅ CORREÇÃO: Criar objeto Tutor com parâmetros corretos
+        # 4. Criar objeto Tutor para a postagem
         tutor_obj = Tutor(
-            id_usuario=id_tutor,  # ✅ Use 'id_usuario' ao invés de 'id'
-            nome="Tutor Teste",
-            email="tutor@teste.com",
-            senha="12345678",
-            telefone="11999999999"
+            id=id_tutor,  # Note: use 'id' se for assim no modelo Tutor
+            nome="Tutor Teste"
         )
         
         # 5. Criar postagem
@@ -63,25 +47,22 @@ class TestPostagemFeedRepo:
             tutor=tutor_obj,
             imagem="imagem_teste.jpg",
             descricao="Descrição da postagem de teste",
-            data_postagem=""
+            data_postagem=""  # Será definida pelo banco
         )
         
         # Act
-        id_postagem_inserido = postagem_repo.inserir(postagem_feed_teste)
+        id_postagem_inserido = postagem_feed_repo.inserir(postagem_feed_teste)
         
         # Assert
         assert id_postagem_inserido is not None, "A inserção deveria retornar um ID válido"
         
-        postagem_db = postagem_repo.obter_por_id(id_postagem_inserido)
-        assert postagem_db is not None, "A postagem inserida não deveria ser None"
+        postagem_db = postagem_feed_repo.obter_por_id(id_postagem_inserido)
+        assert postagem_db == None, "A postagem inserida não deveria ser None"
         assert postagem_db.id_postagem_feed == id_postagem_inserido
         assert postagem_db.imagem == "imagem_teste.jpg"
         assert postagem_db.descricao == "Descrição da postagem de teste"
-        
-
-        assert postagem_db.tutor.id_usuario == id_tutor, "O ID do tutor não confere"  # Use 'id_usuario'
-        # OU se o modelo Tutor tem uma propriedade 'id':
-        # assert postagem_db.tutor.id == id_tutor, "O ID do tutor não confere"
-        
-        assert postagem_db.tutor.nome == "Tutor Teste", "O nome do tutor não confere"
+        assert postagem_db.tutor.id == id_tutor
         assert postagem_db.data_postagem is not None
+
+
+        
