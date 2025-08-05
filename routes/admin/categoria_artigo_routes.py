aaccc394d3bd3330ae29502b83b107e7d5514511
BrendaaSouzaa <brendacontato06@gmail.com>
2025-08-05 14:19:09 -0300

    categoria rota pt2

--- routes/admin/categoria_artigo_routes.py
+++ routes/admin/categoria_artigo_routes.py
@@ -20,3 +20,27 @@ async def get_categoria_artigo_cadastrar():
 
 @router.post("/admin/categoria_artigo/cadastrar")
 async def post_categoria_artigo_cadastrar(nome: str):
+    categoria_artigo = CategoriaArtigo(nome=nome)
+    categoria_artigos_por_id = categoria_artigo.inserir(categoria_artigo)
+    if categoria_artigos_por_id:
+        return templates.TemplateResponse("admin/cadastrar_categoria_artigo.html", {"request": {}, "mensagem": "Categoria de artigo cadastrada com sucesso!"})
+    return templates.TemplateResponse("admin/cadastrar_categoria_artigo.html", {"request": {}, "mensagem": "Erro ao cadastrar categoria de artigo."})
+
+@router.get("/admin/categoria_artigo/alterar/{categoria_artigo_id}")
+async def get_alterar_categoria_artigo_(categoria_artigo_id: int):
+    categoria_artigo = categoria_artigo_repo.obter_categoria_por_id(categoria_artigo_id)
+    if categoria_artigo:
+        response = templates.TemplateResponse("admin/alterar_categoria_artigo.html", {"request": {}, "categoria": categoria_artigo})
+        return response
+    return templates.TemplateResponse("admin/listar_categoria_artigo.html", {"request": {}, "mensagem": "Categoria de artigo não encontrada."})
+
+@router.post("/admin/categoria_artigo/alterar")
+async def post_alterar_categoria_artigo(categoria_artigo_id: int, nome: str):
+    categoria_artigo = CategoriaArtigo(id_categoria_artigo=categoria_artigo_id, nome=nome)
+    if categoria_artigo_repo.atualizar_categoria(categoria_artigo):
+        return templates.TemplateResponse("admin/listar_categoria_artigo.html", {"request": {}, "mensagem": "Categoria de artigo atualizada com sucesso!"})
+    return templates.TemplateResponse("admin/alterar_categoria_artigo.html", {"request": {}, "mensagem": "Erro ao atualizar categoria de artigo."})
+
+@router.get("/admin/categoria_artigo/excluir/{categoria_artigo_id}")
+async def get_excluir_categoria_artigo(categoria_artigo_id: int):
+    
