2041e7c6b2cd3c45766de18c965d527056416c54
LuanaSantosNeves <luanasantosneves718@gmail.com>
2025-08-27 14:04:57 -0300

    ROTAS FINALIZADASS

--- routes/admin/comentario_admin_routes.py
+++ routes/admin/comentario_admin_routes.py
@@ -12,9 +12,6 @@ async def get_root(request: Request):
     response = templates.TemplateResponse("administrador/home_administrador.html", {"request": request})
     return response   
 
-@router.get("/listar_comentarios")
-async def pagina_comentario(request: Request):
-    return templates.TemplateResponse("administrador/listar_comentarios.html", {"request": request})
 
 @router.get("/moderar_comentarios/{id_comentario}")
 async def pagina_comentario(request: Request, id_comentario: int):
